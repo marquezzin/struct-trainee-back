fb6d02bc8f1c6ef5f14542156ef4debcc76c8fdc
leopcortes <leo.pereira.cortes@gmail.com>
2023-08-08 23:57:38 -0300

    updating user

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,7 +1,7 @@
 class UserSerializer < Panko::Serializer
     include Rails.application.routes.url_helpers
 
-    attributes :id, :name, :email, :bio, :is_admin, :profile_picture_url
+    attributes :id, :name, :email, :bio, :is_admin, :profile_picture_url, :points
 
     def profile_picture_url
         rails_blob_path(object.profile_picture, only_path: true) if object.profile_picture.attached?
